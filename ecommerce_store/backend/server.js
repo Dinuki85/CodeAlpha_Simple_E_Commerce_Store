427e1873eee2a348a835e7a720cb88efc8adcdb7
Dinuki85 <prarthanadinuki2@gmail.com>
2024-12-15 21:25:17 +0530

    update:server.js file updated

--- ecommerce_store/backend/server.js
+++ ecommerce_store/backend/server.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 
+dotenv.config();
 const app = express();
 app.use(express.json());
 
-//Database Connection
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,useUnifiedTopology: trur})
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.error(err));
+// Database connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.error(err));
+
+// Routes
+app.use('/api/products', require('./routes/productRoute'));
+app.use('/api/cart', require('./routes/cartRoute'));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
